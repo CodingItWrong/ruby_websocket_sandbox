af98e9867001bd79965827eb8447e21710deae1a
Josh Justice <me@codingitwrong.com>
2021-08-29 09:09:06 -0400

    Test disconnect

--- spec/lib/connection_handler_spec.rb
+++ spec/lib/connection_handler_spec.rb
@@ -59,4 +59,28 @@ RSpec.describe ConnectionHandler do
       perform!
     end
   end
+
+  context 'after disconnecting' do
+    let(:contents) { 'hello world' }
+    let(:other_conn) { double }
+
+    def perform!
+      subject.connected(conn)
+      subject.connected(other_conn)
+      subject.disconnected(conn)
+      subject.received(other_conn, contents)
+    end
+
+    before do
+      allow(messages).to receive(:all)
+        .and_return([])
+      allow(messages).to receive(:create!)
+      allow(other_conn).to receive(:send)
+    end
+
+    it 'does not send messages to the disconnected connection' do
+      expect(conn).not_to receive(:send)
+      perform!
+    end
+  end
 end
