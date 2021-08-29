1078c9eff8aae210a06158625b234e6c60d6f950
Josh Justice <me@codingitwrong.com>
2021-08-29 07:47:09 -0400

    Handle multiple connections

--- lib/app.rb
+++ lib/app.rb
@@ -3,9 +3,13 @@ require_relative 'db'
 require_relative 'message'
 require 'pry'
 
+$connections = Set.new
+
 App = lambda do |env|
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
+    $connections << ws
+
     ws.send('Connected to Faye')
 
     Message.all.each do |message|
@@ -15,12 +19,16 @@ App = lambda do |env|
     ws.on :message do |event|
       contents = event.data
       puts "Faye received: #{contents}"
-      ws.send("Response from Faye: #{contents}")
       Message.create!(contents: contents)
+
+      $connections.each do |connection|
+        connection.send("Response from Faye: #{contents}")
+      end
     end
 
     ws.on :close do |event|
       p [:close, event.code, event.reason]
+      $connections.delete(ws)
       ws = nil
     end
 
