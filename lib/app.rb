a516a9488d2425e4667a9b2105089f0e1fef3e6b
Josh Justice <me@codingitwrong.com>
2021-08-29 07:01:35 -0400

    Extract app file

---
+++ lib/app.rb
@@ -0,0 +1,25 @@
+require 'faye/websocket'
+
+App = lambda do |env|
+  if Faye::WebSocket.websocket?(env)
+    ws = Faye::WebSocket.new(env)
+    ws.send('Connected to Faye')
+
+    ws.on :message do |event|
+      puts "Faye received: #{event.data}"
+      ws.send("Response from Faye: #{event.data}")
+    end
+
+    ws.on :close do |event|
+      p [:close, event.code, event.reason]
+      ws = nil
+    end
+
+    # Return async Rack response
+    ws.rack_response
+
+  else
+    # Normal HTTP request
+    [200, { 'Content-Type' => 'text/plain' }, ['Hello']]
+  end
+end
