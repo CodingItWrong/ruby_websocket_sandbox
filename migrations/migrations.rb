595ef52e63ef3b1c18252453bdde13ce105a2491
Josh Justice <me@codingitwrong.com>
2021-08-31 07:39:40 -0400

    Update migrations to support multiple

--- migrations/migrations.rb
+++ migrations/migrations.rb
@@ -3,9 +3,25 @@
 # TODO: dynamically import all migrations and provide a way to run all
 
 require_relative '00001_create_messages_table.rb'
+require_relative '00002_create_users_table.rb'
 
 class Migrations
-  class << self
-    delegate :migrate, to: CreateMessagesTable
+  MIGRATIONS = [
+    CreateMessagesTable,
+  ]
+
+  def self.migrate(direction)
+    migrations_in_order(direction).each do |m|
+      m.migrate(direction)
+    end
+  end
+
+  def self.migrations_in_order(direction)
+    case direction
+    when :up
+      MIGRATIONS
+    when :down
+      MIGRATIONS.reverse
+    end
   end
 end
